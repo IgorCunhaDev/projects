6390b7b5936baba80dfda2c1203f415683958795
Igor Cunha <igor.cunha916@gmail.com>
2025-04-27 12:14:07 -0300

    temporario
    padrin

---
+++ 12/script.js
@@ -0,0 +1,5 @@
+document.getElementById('confirmBtn').addEventListener('click', () => {
+    const msg = document.getElementById('responseMsg');
+    msg.innerText = 'AEEE! Já estamos preparando os lencinhos pra você chorar junto com a gente! E o pix, é claro. 😭❤️';
+  });
+  
